6d4fc9f5af9b672fcb6a1e202e1582ca7e451b5c
Chris Ivan <ctivan381@gmail.com>
2016-11-01 05:14:42 +0000

    Index and show are up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
+# require "article"
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def index
+    @articles = Article.all
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  def create 
+    
+  end
 end
